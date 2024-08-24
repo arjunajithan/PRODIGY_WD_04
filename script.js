ad275613b85dec2eec4e26c7103a76b6919258f4
Arjun Ajithan Nadukandiyil <arjun.nadukandiyil@bca.christuniversity.in>
2024-08-24 11:44:28 +0530

    .js Script For Landing Page

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Fullstack Developer", "Gamer", "Content Creation"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
